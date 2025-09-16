30b4d102d34c3badfdbb47d5d3e39904404972c1
alinac1888 <alinac44@nycstudents.net>
2025-09-16 09:24:49 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+""" x = ["this", "sentence"]
+print(len("this sentence")) 
+x = float(1.23) 
+y =float(3)
+print(x, y) """
+numbers = [1, 2, 3, 4]
+print(numbers)
+for i in range(2):
+    print(numbers)
