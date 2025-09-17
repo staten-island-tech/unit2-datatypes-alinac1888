45467d7b89d1bc6383eaa688e2a4097cdc4dcd39
alinac1888 <alinac44@nycstudents.net>
2025-09-17 09:26:27 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,34 @@ print(len("this sentence"))
 x = float(1.23) 
 y =float(3)
 print(x, y) """
-numbers = [1, 2, 3, 4]
-print(numbers)
-for i in range(2):
-    print(numbers)
+""" numbers = [1, 2, 3, 4]
+print(numbers[1]) """
+""" friend_comes = True
+money = True
+
+def and_movies(friend, money):
+    if friend and money:
+        print("Going to the movies")
+and_movies(friend_comes, money) """
+
+""" friend_comes = True
+money = True
+
+def or_movies(friend, money):
+    if friend or money:
+        print("Going to the movies")
+and_movies(friend_comes, money) """
+
+""" day_of_week = input("What day is it?")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
+
+print( 4 % 2)
+# remainder is output """
+
+def factor(x, y):
+    if x % y:
+        print("y is a factor of x")
+factor(6734, 64)
